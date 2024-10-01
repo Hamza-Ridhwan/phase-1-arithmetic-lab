9003b6ce18689eaa79c025fdfd17f5a2d04a8d3e
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-10-01 15:11:51 +0300

    Completed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 4;
+
+let num3 = 19;
+let num4 = 5
+let mod = num3 % num4;
+
+let max = 20;
+Math.max(4, 8, 16, 20);
